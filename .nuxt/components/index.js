8ad4291bc63cf4a74eb58fc2e35d9a40ad83c487
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2022-03-24 19:45:10 +0900

    delete .nuxt from git

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,7 +1,7 @@
-export const Header = () => import('../../components/Header.vue' /* webpackChunkName: "components/header" */).then(c => wrapFunctional(c.default || c))
-export const NuxtLogo = () => import('../../components/NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
-export const Tutorial = () => import('../../components/Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
-export const VuetifyLogo = () => import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/vuetify-logo" */).then(c => wrapFunctional(c.default || c))
+export { default as Header } from '../../components/Header.vue'
+export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
+export { default as Tutorial } from '../../components/Tutorial.vue'
+export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {

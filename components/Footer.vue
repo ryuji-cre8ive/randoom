e6750921fe500d2f758a0dab312db4e6dc97ecd7
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2022-12-06 19:24:21 +0900

    ログインコンポーネントを分割しました Footerを追加しました

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer padless align="center">
+    <v-col
+      class="text-center"
+      cols="12"
+      color="white"
+    >
+      {{ new Date().getFullYear() }} — <strong>Ryuji Kokubu</strong>
+    </v-col>
+  </v-footer>
+</template>
---
+++ components/GoogleLogin.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn @click="authenticate" color="#B2DFDB">Googleでログイン<v-icon>mdi-google</v-icon></v-btn>
+</template>
+
+<script>
+export default {
+  auth: false,
+  methods: {
+    authenticate () {
+      this.$auth.loginWith('app')
+    }
+  }
+}
+</script>
